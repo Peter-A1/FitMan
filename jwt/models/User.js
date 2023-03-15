4e41675f6e3f12f6d9e79ea3f5112941f806bc1f
Peter-A1 <peteramir481@gmail.com>
2023-03-15 13:52:04 +0200

    new User model

--- jwt/models/User.js
+++ jwt/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const {isEmail} = require('validator');
+const bcrypt = require('bcrypt');
 
 
 const userSchema = new mongoose.Schema({
@@ -50,6 +51,19 @@ const userSchema = new mongoose.Schema({
         type: Number
     }
 }); 
+
+
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if(user){
+        const auth = await bcrypt.compare(password, user.password);
+        if(auth){
+            return user;
+        }
+        throw Error('incorrect password');
+    }
+    throw Error('incorrect email');
+}
 
 
 userSchema.post('save', function(doc,next){
